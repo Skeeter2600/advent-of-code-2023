ba91748c68c175726910110ae67d1fc6bf9852ba
skeeter2600 <andersbe2600@gmail.com>
2023-12-06 11:37:32 -0500

    finished day 6. On to day 5 :(

--- day6/boat_racing.py
+++ day6/boat_racing.py
@@ -1,5 +1,31 @@
 
 
+def possible_speeds_big(filename):
+    data = []
+
+    with open(filename) as data_values:
+        for line in data_values:
+            line = line.strip().split(':')[1].split()
+
+            if not data:
+                data = ["".join(line)]
+            else:
+                distance = "".join(line)
+                data[0] = [data[0],  distance]
+
+    j = 1
+    possibilities = 0
+    while j < int(data[0][0]):
+        if (int(data[0][0])-j)*j > int(data[0][1]):
+            possibilities += 1
+        else:
+            if possibilities > 0:
+                break
+        j += 1
+
+    return possibilities
+
+
 def possible_speeds(filename):
     speed_product = 0
     data = []
@@ -33,6 +59,7 @@ def possible_speeds(filename):
 
 def main():
     print(possible_speeds('race_data.txt'))
+    print(possible_speeds_big('race_data.txt'))
 
 
 if __name__ == '__main__':
