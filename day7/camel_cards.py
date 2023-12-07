1686357abe99672d88f283d5b97fbc869bddff4c
skeeter2600 <andersbe2600@gmail.com>
2023-12-07 17:51:35 -0500

    actually completed day 7

--- day7/camel_cards.py
+++ day7/camel_cards.py
@@ -15,6 +15,22 @@ CARD_VALUES = {
     'A': 13
 }
 
+JOKER_CARD_VALUES = {
+    'J': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'T': 10,
+    'Q': 11,
+    'K': 12,
+    'A': 13
+}
+
 
 def process_hands(filename):
     hands = []
@@ -27,13 +43,22 @@ def process_hands(filename):
     return hands
 
 
-
-def sort_pile(e):
+def sort_pile_original(e):
     card_num = 0
     i = len(e[0])
     for card in e[0]:
         mult = int(str(1) + str(0)*(i*2))
         card_num += CARD_VALUES[card] * mult
+        i -= 1
+    return card_num
+
+
+def sort_pile_joker(e):
+    card_num = 0
+    i = len(e[0])
+    for card in e[0]:
+        mult = int(str(1) + str(0)*(i*2))
+        card_num += JOKER_CARD_VALUES[card] * mult
         i -= 1
     return card_num
 
@@ -44,6 +69,79 @@ def calc_pile_values(i, pile):
         pile_total += hand[1] * i
         i += 1
     return pile_total
+
+
+def calculate_joker_winnings(filename):
+    five_of_a_kind = []
+    four_of_a_kind = []
+    full_houses = []
+    three_of_a_kind = []
+    two_pairs = []
+    one_pairs = []
+    high_cards = []
+
+    hand_data = process_hands(filename)
+
+    for hand in hand_data:
+        hand_set = list(set(hand[0]))
+        if len(hand_set) == 1:
+            five_of_a_kind.append(hand)
+        elif len(hand_set) == 2:
+            value_count = hand[0].count(hand_set[0])
+            if 'J' in hand_set:
+                five_of_a_kind.append(hand)
+            elif value_count == 1 or value_count == 4:
+                four_of_a_kind.append(hand)
+            else:
+                full_houses.append(hand)
+        elif len(hand_set) == 3:
+            value_count_one, value_count_two, value_count_three = hand[0].count(hand_set[0]), hand[0].count(hand_set[1]), hand[0].count(hand_set[2])
+            if 'J' in hand_set:
+                if hand[0].count('J') != 1:
+                    four_of_a_kind.append(hand)
+                elif value_count_one == 3 or value_count_two == 3 or value_count_three == 3:
+                    four_of_a_kind.append(hand)
+                else:
+                    full_houses.append(hand)
+            elif value_count_one == 3 or value_count_two == 3 or value_count_three == 3:
+                three_of_a_kind.append(hand)
+            else:
+                two_pairs.append(hand)
+        elif len(hand_set) == 4:
+            if 'J' in hand_set:
+                three_of_a_kind.append(hand)
+            else:
+                one_pairs.append(hand)
+        else:
+            if 'J' in hand[0]:
+                one_pairs.append(hand)
+            else:
+                high_cards.append(hand)
+
+    high_cards.sort(key=sort_pile_joker)
+    five_of_a_kind.sort(key=sort_pile_joker)
+    four_of_a_kind.sort(key=sort_pile_joker)
+    full_houses.sort(key=sort_pile_joker)
+    three_of_a_kind.sort(key=sort_pile_joker)
+    two_pairs.sort(key=sort_pile_joker)
+    one_pairs.sort(key=sort_pile_joker)
+
+    i = 1
+    winnings = calc_pile_values(i, high_cards)
+    i += len(high_cards)
+    winnings += calc_pile_values(i, one_pairs)
+    i += len(one_pairs)
+    winnings += calc_pile_values(i, two_pairs)
+    i += len(two_pairs)
+    winnings += calc_pile_values(i, three_of_a_kind)
+    i += len(three_of_a_kind)
+    winnings += calc_pile_values(i, full_houses)
+    i += len(full_houses)
+    winnings += calc_pile_values(i, four_of_a_kind)
+    i += len(four_of_a_kind)
+    winnings += calc_pile_values(i, five_of_a_kind)
+
+    return winnings
 
 
 def calculate_winnings(filename):
@@ -78,13 +176,13 @@ def calculate_winnings(filename):
         else:
             high_cards.append(hand)
 
-    high_cards.sort(key=sort_pile)
-    five_of_a_kind.sort(key=sort_pile)
-    four_of_a_kind.sort(key=sort_pile)
-    full_houses.sort(key=sort_pile)
-    three_of_a_kind.sort(key=sort_pile)
-    two_pairs.sort(key=sort_pile)
-    one_pairs.sort(key=sort_pile)
+    high_cards.sort(key=sort_pile_original)
+    five_of_a_kind.sort(key=sort_pile_original)
+    four_of_a_kind.sort(key=sort_pile_original)
+    full_houses.sort(key=sort_pile_original)
+    three_of_a_kind.sort(key=sort_pile_original)
+    two_pairs.sort(key=sort_pile_original)
+    one_pairs.sort(key=sort_pile_original)
 
     i = 1
     winnings = calc_pile_values(i, high_cards)
@@ -106,6 +204,7 @@ def calculate_winnings(filename):
 
 def main():
     print(calculate_winnings('camel_cards_hand_data.txt'))
+    print(calculate_joker_winnings('camel_cards_hand_data.txt'))
 
 
 if __name__ == '__main__':
