a76c358aad37bd344d64bbc45bb0fa036925d0d3
skeeter2600 <andersbe2600@gmail.com>
2023-12-09 00:35:36 -0500

    completed day 9 part 2. The future is now! easiest part 2 so far

--- day9/oasis_history.py
+++ day9/oasis_history.py
@@ -1,6 +1,6 @@
 
 
-def read_oasis_history(filename):
+def read_oasis_future(filename):
     next_number_total = 0
 
     with open(filename) as patterns:
@@ -33,8 +33,42 @@ def read_oasis_history(filename):
     return next_number_total
 
 
+def read_oasis_past(filename):
+    next_number_total = 0
+
+    with open(filename) as patterns:
+        for pattern in patterns:
+            pattern = pattern.split()
+            pattern_layers = [pattern]
+            break_cond = True
+
+            # do layers
+            while break_cond:
+                i = 0
+                current_diffs = []
+                while i < len(pattern)-1:
+                    current_diffs.append(int(pattern[i+1]) - int(pattern[i]))
+                    i += 1
+                if not any(current_diffs):
+                    pattern_layers.append(current_diffs)
+                    break_cond = False
+                else:
+                    pattern_layers.append(current_diffs)
+                    pattern = current_diffs
+
+            # calculate next
+            value = 0
+            for element in pattern_layers[::-1]:
+                value = int(element[0]) - value
+
+            next_number_total += value
+
+    return next_number_total
+
+
 def main():
-    print(read_oasis_history('oasis_data.txt'))
+    print(read_oasis_future('oasis_data.txt'))
+    print(read_oasis_past('oasis_data.txt'))
 
 
 if __name__ == '__main__':
