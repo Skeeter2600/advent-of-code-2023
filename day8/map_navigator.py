e8c5492df5be81e751f9e9c67407f4de8cd6a61c
skeeter2600 <andersbe2600@gmail.com>
2023-12-09 00:00:49 -0500

    completed day 8 part 2. To the sun and back

--- day8/map_navigator.py
+++ day8/map_navigator.py
@@ -1,7 +1,10 @@
+from math import gcd
+
 
 def map_reader(filename):
     instructions = ""
     map_data = {}
+    ends_in_a = []
 
     with open(filename) as map_info:
         for line in map_info:
@@ -11,9 +14,46 @@ def map_reader(filename):
             elif line:
                 line = line.split('=')
                 left_and_right = line[1].strip().split(',')
+                if line[0][2] == 'A':
+                    ends_in_a.append(line[0].strip())
                 map_data[line[0].strip()] = {'L': left_and_right[0].strip()[1:], 'R': left_and_right[1].strip()[:-1]}
 
-    return [instructions, map_data]
+    return [instructions, map_data, ends_in_a]
+
+
+def ghost_navigate(filename):
+    processed_map = map_reader(filename)
+    instructions, map_data, ghost_starts = processed_map[0], processed_map[1], processed_map[2]
+
+    ghost_steps = []
+
+    """
+    NOTE FOR LATER:
+    Calculate how many steps to find the Z,
+    then fund least common multiplier between them all
+    THere are 6 values, find the LCM between 1, 2, 3, 4, 5, and 6
+    """
+
+    for element in ghost_starts:
+        position = element
+        steps = 0
+        attempts = 0
+        breaking = True
+        while attempts < 1000 and breaking:  # just a safety thing
+            for char in instructions:
+                steps += 1
+                position = map_data[position][char]
+                if position[2] == 'Z':
+                    print(element + " " + str(steps))
+                    ghost_steps.append(steps)
+                    breaking = False
+        attempts += 1
+
+    lcm = 1
+
+    for i in ghost_steps:
+        lcm = lcm*i//gcd(lcm, i)
+    return lcm
 
 
 def navigate(filename):
@@ -38,6 +78,7 @@ def navigate(filename):
 
 def main():
     print(navigate('map_data.txt'))
+    print(ghost_navigate('map_data.txt'))
 
 
 if __name__ == '__main__':
